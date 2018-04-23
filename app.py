bedc16ac7eced9fbed6c9bdac22be7c3b0720218
amanfojnr <kenmarch@zoho.com>
2018-04-23 23:46:02 +0000

    version one done

--- app.py
+++ app.py
@@ -7,15 +7,37 @@
 # Expl00ra <amanfojnr@zoho.com>
 
 import os
-import configparser
 import time
+from random import randrange
 
 import tweepy
 
-config = configparser.ConfigParser()
-config.read("config.ini")
+consumer_key = os.environ["CONSUMER_KEY"]
+consumer_secret = os.environ["CONSUMER_SECRET"]
+access_token = os.environ["ACCESS_TOKEN"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 
-consumer_key = config["API_KEYS"]["consumer_key"]
-consumer_secret = config["API_KEYS"]["consumer_secret"]
-access_token = config["API_KEYS"]["access_token"]
-access_token_secret = config["API_KEYS"]["access_token_secret"]
+# setup app
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+
+def main():
+    """
+    Retweets and favorites 2 picked tweets randomly from
+    timeline
+    """
+    while True:
+        home_tweets = api.home_timeline()[:10]  # pick top 10 tweets in newsfeed
+        status1 = api.retweet(home_tweets[randrange(0, 10)].id)  # retweet one at random
+        print(status1.text + " -- rt")
+        time.sleep(10)  # wait for 10s
+        status2 = api.create_favorite(home_tweets[randrange(0, 10)].id)  # favorite one at random
+        print(status2.text + " -- <3")
+        time.sleep(1800)  # wait for 30mins
+
+if __name__ == "__main__":
+    print("afiax --> tweetator has started")
+    main()
